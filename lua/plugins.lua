9891b180e460621e524f56ebe48546de2d353a77
ekapilik <erickapilik@gmail.com>
2023-05-27 19:16:27 -0700

    add autocompletion

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -75,7 +75,7 @@ function M.setup()
       end,
     }
 
-        -- Better icons
+    -- Better icons
     use {
       "kyazdani42/nvim-web-devicons",
       module = "nvim-web-devicons",
@@ -83,6 +83,7 @@ function M.setup()
         require("nvim-web-devicons").setup { default = true }
       end,
     }
+
 
     -- Better Comment
     use {
@@ -146,6 +147,7 @@ function M.setup()
       requires = { "kyazdani42/nvim-web-devicons" },
     }
 
+    -- file explorer
     use {
      "kyazdani42/nvim-tree.lua",
      requires = {
@@ -156,9 +158,56 @@ function M.setup()
      end,
     }
 
-    -- ---------------------------------------------
-    -- ------------------ RESTART ------------------
-    -- ---------------------------------------------
+    -- Completion
+    use {
+      "ms-jpq/coq_nvim",
+      branch = "coq",
+      event = "InsertEnter",
+      run = ":COQdeps",
+      config = function()
+        require("config.coq").setup()
+      end,
+      requires = {
+        { "ms-jpq/coq.artifacts", branch = "artifacts" },
+        { "ms-jpq/coq.thirdparty", branch = "3p", module = "coq_3p" },
+      },
+      disable = false,
+    }
+
+    -- nvim-cmp
+    use {
+      "hrsh7th/nvim-cmp",
+      event = "InsertEnter",
+      opt = true,
+      config = function()
+        require("config.cmp").setup()
+      end,
+      wants = { "LuaSnip" },
+      requires = {
+        "hrsh7th/cmp-buffer",
+        "hrsh7th/cmp-path",
+        "hrsh7th/cmp-nvim-lua",
+        "ray-x/cmp-treesitter",
+        "hrsh7th/cmp-cmdline",
+        "saadparwaiz1/cmp_luasnip",
+        "hrsh7th/cmp-calc",
+        "f3fora/cmp-spell",
+        "hrsh7th/cmp-emoji",
+        {
+          "L3MON4D3/LuaSnip",
+          wants = "friendly-snippets",
+          config = function()
+            require("config.luasnip").setup()
+          end,
+        },
+        "rafamadriz/friendly-snippets",
+        disable = false,
+      },
+    }
+
+    --------------------------------------------
+    ----------------- RESTART ------------------
+    --------------------------------------------
     if packer_bootstrap then
       print "Restart Neovim after installation!"
       require("packer").sync()
